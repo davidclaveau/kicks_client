633d516a371845b967337899570c21f635306701
davidclaveau <daveclaveau@gmail.com>
2021-08-13 11:36:52 -0700

    Create the schedule component with game info

---
+++ src/pages/Game.jsx
@@ -0,0 +1,26 @@
+import Typography from '@material-ui/core/Typography'
+
+const Game = (props) => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Game Date</th>
+          <th>Game Time</th>
+          <th>Away Team</th>
+          <th>Home Team</th>
+          <th>Field</th>
+        </tr>
+        <tr>
+          <td>{props.gameDate}</td>
+          <td>{props.gameTime}</td>
+          <td>{props.awayTeam}</td>
+          <td>{props.homeTeam}</td>
+          <td>{props.field}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Game
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,9 +1,49 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Game from './Game'
+
 const Schedule = () => {
+  const [schedule, setSchedule] = useState([])
+
+  const apiURL = 'http://localhost:3001/api/v1'
+  
+  useEffect(() => {
+    getSchedule()
+  }, [])
+  
+  async function getSchedule() {
+    try {
+      const response = await axios.get(`${apiURL}/schedules`);
+      setSchedule(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log("schedule", schedule)
+
+  const games = schedule.map(game => {
+    return (
+      <Game 
+        key={game.id}
+        awayTeam={game.away_team.name}
+        homeTeam={game.home_team.name}
+        field={game.field}
+        gameTime={game.game_time}
+        gameDate={game.game_date}
+      />
+    )
+  })  
+
   return (
     <div>
       This is the Schedules component.
+      <ul>
+        {games}
+      </ul>
     </div>
   )
-}
+}; 
 
 export default Schedule;
