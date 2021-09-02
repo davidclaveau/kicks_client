d1438ca3dfafb222b4f5fdc940895d11ce572b9b
davidclaveau <daveclaveau@gmail.com>
2021-09-02 16:40:38 -0700

    Add logic where remove player only shows for admins

--- src/pages/RosterPlayer.jsx
+++ src/pages/RosterPlayer.jsx
@@ -1,14 +1,17 @@
 import { IconButton } from '@material-ui/core';
 import CancelIcon from '@material-ui/icons/Cancel';
+import { useContext } from 'react';
+import { UserContext } from '../contexts/userContext';
 
 const AddPlayer = (props) => {
-
+  const { user } = useContext(UserContext);
+  
   return (
     <tr>
       <td>{props.firstName} {props.lastName}</td>
       <td>{props.winterTeam}</td>
       <td>{props.publicSector}</td>
-      {props.onRemove &&
+      {(user.role === "Admin") && props.onRemove &&
         <td>
           <IconButton>
             <CancelIcon onClick={props.onRemove} color="secondary"/>
